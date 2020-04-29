22b98b16b15210b8a301a1dfd3db956f09cdf627
Cláudia Castiço <claudiacastico@gmail.com>
2020-04-29 17:50:32 +0100

    Starting to implement the restful API

--- src/main/java/org/academiadecodigo/hackstreetboys/ApplePieDating/controller/MainController.java
+++ src/main/java/org/academiadecodigo/hackstreetboys/ApplePieDating/controller/MainController.java
@@ -1,4 +1,45 @@
 package org.academiadecodigo.hackstreetboys.ApplePieDating.controller;
 
-public class MainController {
+@RestController
+@RequestMapping("/api")
+public class RestIndexController {
+
+    /**
+     * Retrieves the API name and version
+     *
+     * @return the response
+     */
+    @RequestMapping(method = RequestMethod.GET, path = {"/", ""})
+    @ResponseBody
+    protected ApiVersion showVersion() {
+
+        ApiVersion version = new ApiVersion();
+        version.setName("ApplePieDating API");
+        version.setVersion("v0.1");
+
+        return version;
+
+    }
+
+    private static class ApiVersion {
+
+        private String name;
+        private String version;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getVersion() {
+            return version;
+        }
+
+        public void setVersion(String version) {
+            this.version = version;
+        }
+    }
 }
