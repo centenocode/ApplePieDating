5020aab7b2d6422a069d962f914b6529469df582
Lucas Centeno <centenocode@gmail.com>
2020-04-29 13:10:43 +0100

    matchs working in JS, saving matching profiles

--- newApplePieJS/applepiescript.js
+++ newApplePieJS/applepiescript.js
@@ -1,13 +1,11 @@
 
 var users = [
-    { Name: "Lucas Sperb", picture: "<br><img src=https://pbs.twimg.com/profile_images/1049240766692974593/VlfspzfG_400x400.jpg></img>", preferences: [0,2,4,6,8,10,12,14,16,18,20]},
-    { Name: "Claudia", picture: "<br><img src=https://pbs.twimg.com/profile_images/806082380226265088/0wvLjeNW_400x400.jpg></img>", preferences: [1,3,5,7,9,11,13,15,17,19]},
-    { Name: "Uttam", picture: "<br><img src=https://pbs.twimg.com/profile_images/968232588958560256/IfOqS8l2_400x400.jpg></img>", preferences: [0,3,5,7,9,10,12,15,17,20]},
+    {ID: 9876, Name: "Lucas Sperb", picture: "<br><img src=https://pbs.twimg.com/profile_images/1049240766692974593/VlfspzfG_400x400.jpg></img>", preferences: [0,2,4,6,8,10,12,14,16,18,20], matches: []},
+    {ID: 8765, Name: "Claudia", picture: "<br><img src=https://pbs.twimg.com/profile_images/806082380226265088/0wvLjeNW_400x400.jpg></img>", preferences: [1,3,5,7,9,11,13,15,17,19], matches: []},
+    {ID: 7654, Name: "Uttam", picture: "<br><img src=https://pbs.twimg.com/profile_images/968232588958560256/IfOqS8l2_400x400.jpg></img>", preferences: [0,3,5,7,9,10,12,15,17,19], matches: []}
 ];
 
-var currentUser = {Name: "Lucas Sperb", picture: "<br><img src=https://pbs.twimg.com/profile_images/1049240766692974593/VlfspzfG_400x400.jpg></img>", preferences: [0,2,4,6,8,10,12,14,16,18,20]};
-
-
+var currentUser = {ID: 6543, Name: "Vasco", picture: "<br><img src=https://pbs.twimg.com/profile_images/1049240766692974593/VlfspzfG_400x400.jpg></img>", preferences: [0,2,4,6,8,10,12,14,16,18], matches: []};
 
 var clickedElements = [];
 
@@ -17,46 +15,51 @@ $(document).ready(function() {
 
     var children = $("tr").children();
 
-    console.log(Array.isArray(children));
-
     $("a").click(function(event) {
-
-
-
         alert("Nah, nothing to see here.");
         $("a").text("lol");
         $("a").css("font-weight", "bold");
-   
     });
+
+    function howMuchofAMatch(a, b) {
+        return a.filter(function(i) {
+          return b.includes(i);
+        }).length;  
+      }
 
     $("#calculate").click(function(event) {
+        console.log('I was clicked');
+        for(var i = 0; i < users.length; i++){
+        var score = howMuchofAMatch(currentUser.preferences, users[i].preferences);
+            console.log('score' + score);
 
-        var coolGuyPic = users.forEach.preferences.filter(function(el) {
-            return currentUser.preferences.indexOf(el) >= 0;
-        }).length;
+            console.log(currentUser.matches);
 
-       // users.forEach.preferences(globalpreferences=> currentUser.preferences.forEach == (globalpreferences.forEach));
+            console.log('user ID' + users[i].ID);
+            if(score==10){
+            currentUser.matches.push(users[i].ID)
+            console.log(currentUser.matches);
 
-      //  array1.forEach(element => console.log(element));
- 
+        }
+    }
 
-    
-           
+         matchhtml ="<h1>" +  + "</h1>";
+         $(matchhtml).appendTo("#calculate");
          $("<br><img src=https://ak7.picdn.net/shutterstock/videos/17965987/thumb/6.jpg></img>");
- 
-        $(coolGuyPic).appendTo("p");
-   
+
     });
-   
+    
+    function populateTableWithUsers(data) {
+
+        data.forEach(function(users) {
+            users =
+                "<tr><td>" + users.Name + "</td>" +
+                "<td>" + users.ID + "</td>" +
+                "<td>" + users.matches + "</td>" +
+                "<td>" + users.picture + "</td>" +
+                "<td>" + users.preferences + "</td></tr>";
+            $(users).appendTo("#user-data");
+        });
+    };
 });
 
-function populateTableWithUsers(data) {
-
-    data.forEach(function(users) {
-        users =
-            "<tr><td>" + users.Name + "</td>" +
-            "<td>" + users.picture + "</td>" +
-            "<td>" + users.preferences + "</td></tr>";
-        $(users).appendTo("#user-data");
-    });
-};
