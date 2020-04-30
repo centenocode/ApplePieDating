8ec184d99223eb0352356ebe158b19f5742b744d
Uttam Gurung <legendhell2@gmail.com>
2020-04-30 04:37:05 +0100

    new index page created

--- templates/app.js
+++ templates/app.js
@@ -1,8 +1,8 @@
 var pic = ["images/footballgd.jpg", "images/sushi.png", "images/noise.png", "images/dontlisten1.png",
-  "images/party.png", "images/milk.png", "images/pinapple.png", "images/drive.png"
+  "images/party.png", "images/milk.png", "images/pinapple.png", "images/drive.png", "images/column.png", "images/atm-line.png"
 ];
-var questions = ["Do you like Football", "Are you a sushi lover?", " Do you ever feel that people talk too much?",
-  "Do you ever feel that you're talking and nobody is listening?", "Do you like to party?", "Do you believe that people that put the milk before the cereals are mad?", "How do you feel about pineaple in pizza?", "How do you feel about people that drive on the left lane at 30km per hour?"
+var questions = ["Do you like Football?", "Are you a sushi lover?", " Do you ever feel that people talk too much?",
+  "Do you ever feel that you're talking and nobody is listening?", "Do you like to party?", "Do you believe that people that put the milk before the cereals are mad?", "How do you feel about pineaple in pizza?", "How do you feel about people that drive on the left lane at 30km per hour?", "How do you feel about people that listen to music in public<br> transports with a column instead of headphones?", "How do you feel about people that go to the ATM pay for 30 bills,<br> ahead of you, and you just want to withdraw money?"
 ];
 
 var heartBtn = document.querySelector(".heart");
